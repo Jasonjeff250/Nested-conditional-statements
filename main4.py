3199ab7b34a3c15200c569612e038f229a07f486
Jasonjeff250 <etimitah@gmail.com>
2025-12-06 15:03:04 +0100

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,5 @@
+entry = input("Enter anything: ")
+if len(entry) == 1 and entry.isalpha():
+    print("It is a letter.")
+else:
+    print("It is not a letter.")
---
+++ main5.py
@@ -0,0 +1,6 @@
+#Code to check whether user is over 21 or not
+age = int(input("Enter your age: "))
+if age >= 21:
+    print("You are eligible to enter.")
+else:
+    print("You are not eligible to enter.")
