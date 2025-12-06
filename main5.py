9670d0e314a99648005eb6a55e7a0067b8b91b9d
Jasonjeff250 <etimitah@gmail.com>
2025-12-06 15:12:35 +0100

    Add files via upload

--- main5.py
+++ main5.py
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 #Code to check whether user is over 21 or not
 age = int(input("Enter your age: "))
 if age >= 21:
     print("You are eligible to enter.")
 else:
+=======
+#Code to check whether user is over 21 or not
+age = int(input("Enter your age: "))
+if age >= 21:
+    print("You are eligible to enter.")
+else:
+>>>>>>> 3199ab7b34a3c15200c569612e038f229a07f486
     print("You are not eligible to enter.")
